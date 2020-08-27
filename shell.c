9cc5ed439aaa0f7eb905726759f03402157a08e3
WashHolanda <holanda.wash@gmail.com>
2020-08-26 21:12:07 -0300

    PDF descrevendo o que será feito no trabalho e o arquivo .c base

---
+++ shell.c
@@ -0,0 +1,18 @@
+/*
+Aluno: André Luis Gonçalves Bien
+RA: 111829 Turma: Integral
+Aluno: Andrew Medeiros de Campos
+RA: 111775 Turma: Noturno
+Aluno: Washington Holanda de Oliveira
+RA: 112268 Turma: Noturno
+TRABALHO 1: Implementação de um Shell
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
