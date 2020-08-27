10a3d8783faa729394fc2a159fdd4858deb54004
AndrewCampos <a.campos98@live.com>
2020-08-27 14:16:34 -0300

    Arrumada alocaçao do descritor de arquivos

--- shell.c
+++ shell.c
@@ -19,49 +19,70 @@ Aluno: Washington Holanda de Oliveira
 #include <fcntl.h>
 #define TRUE 1
 #define FALSE 0
-#define TAM_LINHA 30 // tamanho máximo da linha nos 
+#define TAM_LINHA 30 // tamanho máximo dos comandos
 
 int main(){
     char **comando, in[100], *token;
-    char **fd; // descritor de arquivos
+    int **fd; // descritor de arquivos
     int cmd_div[30]; // vetor com a posição de cada pipe
     int tam = 0; // quantidade de linhas da matriz
     int i,j; // contador
     int flag_exit = FALSE; // flag de controle
-    pid_t pid;
+    pid_t child; // processo filho
 
-    while(flag_exit == FALSE){ // executa o shell enquanto não é inserido o comando exit
+    scanf("%[^\n]s",in); // scanf() guarda a linha toda na string in
+    token = strtok(in," "); // tokeniza a entrada usando espaço como separador
+    comando = (char**) calloc(TAM_LINHA*sizeof(char*), 1);
 
-        scanf("%[^\n]s",in); // scanf() guarda a linha toda na string in
-        token = strtok(in," "); // tokeniza a entrada usando espaço como separador
-        comando = (char**) calloc(TAM_LINHA*sizeof(char*), 1);
-
-        // insere cada palavra em uma linha da matriz de comandos
-        i = 0;
-        j = 0;
-        while(token != NULL){
-            comando[i] = (char*) calloc(strlen(token)*sizeof(char**), 1);
-            // parser para encontar os pipes
-            if(strcmp(token,"|") == 0){
-                cmd_div[j] = i;
-                strcpy(comando[i],NULL);
-                i++;
-                j++;
-            }else{
-                strcpy(comando[i],token);
-                i++;
-            }
-            tam++;
-            token = strtok(NULL," ");
+    // insere cada palavra em uma linha da matriz de comandos
+    i = 0;
+    j = 1;
+    cmd_div[0]=0;
+    while(token != NULL){
+        comando[i] = (char*) calloc(strlen(token)*sizeof(char*), 1);
+        // parser para encontar os pipes
+        if(strcmp(token,"|") == 0){
+            cmd_div[j] = i+1;
+            comando[i] = NULL; // o pipe precisa ser null para o execvp() saber onde parar de executar o comando
+            j++;
+        }else{
+            strcpy(comando[i],token); // copia palavra do comando para a matriz
         }
-        j=0;
+        i++;
+        tam++;
+        token = strtok(NULL," ");
+    }
 
-        // alocação do descritor de arquivos
+    // alocação do descritor de arquivos
+    fd = malloc(j*sizeof(int*));
+    for(i=0;i<j;i++){
+        fd[i] = malloc(2*sizeof(int));
+    }
+    for(i=0;i<j;i++){
+        pipe(fd[i]); // cria um pipe para cada descritor de arquivos
+    }
 
-        // processa os comandos
-        pid = fork(); // cria processo filho
-        if(pid == 0){ // confere se o processo é filho
-                        
+    for(i=0;i<j;i++){ // j guarda a quantiade de forks necessários
+        child = fork();
+        if(child == 0){
+            int FILE_out, FILE_in; // arquivos para redirecionamento de entrada ou saida
+            int indice = cmd_div[i];
+            if(i != 0){
+                close(fd[i-1][1]); // fecha o descritor de escrita
+                dup2(fd[i-1][0], STDIN_FILENO); // muda o descritor de leitura para stdin
+				close(fd[i-1][0]); // fecha o descritor de leitura
+            }
+            if(i != j){
+                close(fd[i][0]); // fecha o descritor de leitura
+				dup2(fd[i][1], STDOUT_FILENO); // muda o descritor de escrita para stdout
+				close(fd[i][1]); // fecha o descritor de escrita
+            }
+            printf("Comando: %s\n",comando[indice]);
+            execvp(comando[indice],&comando[indice]);
+
+        }else{
+            int status;
+            waitpid(-1,&status,0); // pai espera ate o fim da execussão dos filhos
         }
     }
     return 0;
